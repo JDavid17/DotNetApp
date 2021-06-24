e2cd61ef8bc7b709643c7d97f8111c1d666a0d69
Joel David Hernández Cruz <jdavidhc1710@gmail.com>
2021-06-24 10:01:58 -0400

    Added Cors

--- Startup.cs
+++ Startup.cs
@@ -18,7 +18,11 @@ namespace WebApplication5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins", 
+                    builder => builder.AllowAnyOrigin());
+            });
             services.AddControllers();
         }
 
